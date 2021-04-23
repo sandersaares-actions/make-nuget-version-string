a331eb910e5958bb2d499930cddde948b3f294ed
Sander Saares <sasaares@microsoft.com>
2021-04-23 12:30:17 +0300

    Target sandersaares.devops-scripts

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,18 +1,16 @@
 $ErrorActionPreference = "Stop"
 
-Import-Module Axinom.DevOpsTooling
+Import-Module sandersaares.devops-scripts
 
 if ($env:INPUT_RELEASETYPE -eq "stable") {
-    $version = Set-NuGetVersionString -buildNumber $env:INPUT_VERSIONSTRING -stableVersion
+    Set-NuGetVersionString -buildNumber $env:INPUT_VERSIONSTRING -stableVersion
 }
 elseif ($env:INPUT_RELEASETYPE -eq "preview") {
-    $version = Set-NuGetVersionString -buildNumber $env:INPUT_VERSIONSTRING -previewVersion
+    Set-NuGetVersionString -buildNumber $env:INPUT_VERSIONSTRING -previewVersion
 }
 elseif ($env:INPUT_RELEASETYPE -eq "cb") {
-    $version = Set-NuGetVersionString -buildNumber $env:INPUT_VERSIONSTRING
+    Set-NuGetVersionString -buildNumber $env:INPUT_VERSIONSTRING
 }
 else {
     Write-Error "Unsupported releaseType attribute: $($env:INPUT_RELEASETYPE)"
 }
-
-Write-Host "::set-output name=nugetVersionstring::$version"
